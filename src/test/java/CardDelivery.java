0a59e9c0b025c2f3ba77aa9073645ad70fa2cc3e
Rinat Akhmetzyanov <rin.akhm@gmail.com>
2020-02-05 22:21:35 +0300

    comment teacher 1

--- src/test/java/CardDelivery.java
+++ src/test/java/CardDelivery.java
@@ -1,7 +1,5 @@
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
@@ -15,21 +13,6 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class CardDelivery {
 
-    @Test
-    void test2() {
-        open("http://localhost:7777");
-        $("input[placeholder='Город']").setValue("Мо");
-        SelenideElement element = $$("div>[class='popup__inner']>[class='popup__content']").last();
-        element.$(byText("Москва")).click();
-        $("input[name='name']").setValue("Ринат Ринатов");
-        $("input[placeholder='Дата встречи']").setValue(getFutureDate(3));
-        $("input[placeholder='Дата встречи']~[class]").click();
-        $("input[name='phone']").setValue("+79100000000");
-        $(".checkbox__text").click();
-        $(".button__icon~.button__text").click();
-        $(withText("Успешн")).waitUntil(Condition.visible, 11000);
-    }
-
     SelenideElement form = $("form");
     SelenideElement city = form.$("input[placeholder='Город'");
     SelenideElement date = form.$("input[placeholder='Дата встречи']");
@@ -38,7 +21,6 @@ public class CardDelivery {
     SelenideElement checkMark = form.$(".checkbox__text");
     SelenideElement button = $$(".button__icon~.button__text").find(exactText("Забронировать"));
     SelenideElement notification = $(withText("Успешн"));
-   // private Condition visible;
 
     private String getFutureDate(int plusDate) {
         DateTimeFormatter format = DateTimeFormatter.ofPattern("dd.MM.YYYY");
@@ -49,14 +31,13 @@ public class CardDelivery {
     }
 
 
-
     @BeforeEach
     void openLocalhost() {
-        open("http://localhost:7777");
+        open("http://localhost:9999");
     }
 
     @Test
-    void shouldTest() {
+    void shouldReservationCard() {
         city.setValue("Москва");
         date.doubleClick().sendKeys(Keys.BACK_SPACE);
         date.setValue(getFutureDate(3));
@@ -67,6 +48,8 @@ public class CardDelivery {
         notification.waitUntil(exist, 15000);
     }
 }
+
+
 
 
 
